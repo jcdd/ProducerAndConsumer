c030556b6415abf91c5828e3265e385369134e1e
jcdd <404054065@qq.com>
2014-05-24 22:24:26 +0800

    Add from Web
    Add from Web

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include<string>
 #include<stdlib.h>
 int main() {
+  /*
+  *web github haha
+  */
   string item ="apple";
   ///缓冲区大小设为150，3个生产者，3个消费者
   ProducerConsumer<string>* procon = new ProducerConsumer<string>(150 ,3, 3);
