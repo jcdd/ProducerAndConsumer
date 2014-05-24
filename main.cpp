9676f5eaec80c0f208f31062d2981699757a7024
jcdd <404054065@qq.com>
2014-05-24 23:07:14 +0800

    fouth commit

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@ int main() {
   /*
   *web github haha
   */
+ /*
+  * @jcdd
+  */
   string item ="apple";
-  ///缓冲区大小设为150，3个生产者，3个消费者
+  ///缂撳啿鍖哄ぇ灏忚涓�50锛�涓敓浜ц�锛�涓秷璐硅�
   ProducerConsumer<string>* procon = new ProducerConsumer<string>(150 ,3, 3);
   procon->Produce(item);
   procon->Consume();
